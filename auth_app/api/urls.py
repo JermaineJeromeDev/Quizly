b42566c34d8b625a517e3875cd1443dc4e93e469
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-06-04 16:34:43 +0200

    Add logout URL path to API routes

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import login_user, register_user
+from .views import login_user, logout_user, register_user
 
 urlpatterns = [
     path("register/", register_user, name="register"),
     path("login/", login_user, name="login"),
+    path("logout/", logout_user, name="logout"),
 ]
