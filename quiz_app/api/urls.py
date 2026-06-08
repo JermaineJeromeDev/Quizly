0b98737ac9624a62353dffd60ddcd205c1b9b837
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-06-08 15:10:58 +0200

    Add quiz detail view URL routing to urlpatterns

--- quiz_app/api/urls.py
+++ quiz_app/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import create_quiz_view
+from .views import create_quiz_view, quiz_detail_view
 
 urlpatterns = [
     path("quizzes/", create_quiz_view, name="quiz_list_create"),
+    path("quizzes/<int:quiz_id>/", quiz_detail_view, name="quiz_detail"),
 ]
