da565fdf3fd8c5de4b4f46b4d001d3120b431cdf
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-06-08 14:47:37 +0200

    Add serializers for Question and Quiz models to format API responses

---
+++ quiz_app/api/serializers.py
@@ -0,0 +1,35 @@
+from quiz_app.models import Question, Quiz
+from rest_framework import serializers
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    """Konvertiert die Fragen-Objekte exakt in das geforderte JSON-Format."""
+
+    class Meta:
+        model = Question
+        fields = [
+            "id",
+            "question_title",
+            "question_options",
+            "answer",
+            "created_at",
+            "updated_at",
+        ]
+
+
+class QuizSerializer(serializers.ModelSerializer):
+    """Konvertiert das Quiz samt verschachtelten Fragen fuer die API-Response."""
+
+    questions = QuestionSerializer(many=True, read_ok=True)
+
+    class Meta:
+        model = Quiz
+        fields = [
+            "id",
+            "title",
+            "description",
+            "created_at",
+            "updated_at",
+            "video_url",
+            "questions",
+        ]
